6e11a46f27b8229871ce8f526178d80505d67069
muradtaghiyev05 <muradtaghiyev05@gmail.com>
2023-01-14 22:06:17 +0400

    fixes

--- src/watches/data.jsx
+++ src/watches/data.jsx
@@ -73,7 +73,6 @@ const watchesArr = [
         id: 1,
         title: "Classic Cornwall",
         price: 209.00,
-        quantity: 1,
         discount: 0,
         desc: "With its distinctive dial and all-black NATO strap, the Classic Cornwall is a true eye-catcher that embodies the timeless and elegant characteristics of our flagship Classic collection. With a sleek design and a captivating essence, this is a modern Classic made for every occasion.",
         images: {
@@ -88,7 +87,6 @@ const watchesArr = [
         id: 2,
         title: "Tank Louis Cartier",
         price: 700.00,
-        quantity: 1,
         discount: 0,
         desc: "Tank Louis Cartier watch, large model, quartz movement. Case in 18K yellow gold (750/1000), beaded crown set with a sapphire, grained silvered dial, blued-steel sword-shaped hands, mineral crystal, strap in dark gray alligator skin, ardillon buckle in 18K yellow gold (750/1000). Case dimensions: 33.7 mm x 25.5 mm, thickness: 6.3 mm. Water-resistant up to 3 bar (30 meters/100 feet).",
         images: {
@@ -103,7 +101,6 @@ const watchesArr = [
         id: 3,
         title: "Fossil Carlie Mini",
         price: 65.00,
-        quantity: 1,
         discount: 0,
         desc: "Case size: 28mm; Band size: 12mm; quartz movement with 3-hand analog display; hardened mineral crystal lens resists scratches; imported. Gold- tone stainless steel case with white satin dial; brown eco leather cuff band with adjustable buckle closure; our eco leather uses a resource - efficient tanning process",
         images: {
@@ -117,7 +114,6 @@ const watchesArr = [
         id: 4,
         title: "Hublot",
         price: 820.00,
-        quantity: 1,
         discount: 25,
         desc: "Football, or Soccer as it is known in the US, a worldwide sport and universal language. From 2006, Hublot has been the first Fine Watchmaking brand to join with the sport. A partnership which is enhanced by a unanimous spirit of tenacity, technique, competition and a search for excellence. Ever since, Hublot has shared the emotions of victories with teams and the public throughout every major competition, alongside FIFA and UEFA as the Official Timekeeper and Official Watch of prestigious clubs Juventus, Chelsea, AFC Ajax, Benfica and famous personalities from the sport, Pelé, José Mourinho and Kylian Mbappé.",
         images: {
@@ -132,7 +128,6 @@ const watchesArr = [
         id: 5,
         title: "Patek Philippe",
         price: 957.00,
-        quantity: 1,
         discount: 0,
         desc: "In 2011, Reference 7130 set the crowning touch to the longstanding tradition of Patek Philippe World Time watches by introducing a ladies’ model. In 2022, the Manufacture reinterprets this classic by adorning it with an original, modern and elegant olive green color. The dial center pays tribute to Rare Craftsmanship skills with a finely hand-guilloched basketweave motif. Its olive green shade is picked up on the city disk on which the white-printed names stand out in an extremely legible manner.",
         images: {
@@ -146,7 +141,6 @@ const watchesArr = [
         id: 6,
         title: "Vitae London Elmington",
         price: 299.00,
-        quantity: 1,
         discount: 54,
         desc: "A minimalist sunray dial with second sub-eye defines this stylish watch.",
         images: {
@@ -160,7 +154,6 @@ const watchesArr = [
         id: 7,
         title: "Skagen",
         price: 145.00,
-        quantity: 1,
         discount: 0,
         desc: "From the coastlines of Skagen to the architecture of Copenhagen and a celebrated fashion scene, Denmark embodies a special kind of energy: modern, minimalist, and playful. In the moment— with an appreciation for nature and the wild world around us. The Skagen Jorn aesthetic is simple, and works with every style.Slip on one of our men's watches while you're getting ready for any occasion and you'll see your refined style take on a whole new level of sophistication.",
         images: {
@@ -175,7 +168,6 @@ const watchesArr = [
         id: 8,
         title: "Timex Waterbury",
         price: 159.00,
-        quantity: 1,
         discount: 13,
         desc: "We have looked to our past, and we’re paying homage to our original watch collection by using the Waterbury Watch Company logo on the dial, crown and second hand, which bears the stylized W. Each watch displays our attention to craftsmanship and detail, with finely-finished surfaces and careful applications of watchmaking ingenuity. Featuring a classic green dial and a rich caramel brown leather strap, our Waterbury Traditional Chronograph unites all of these elements into something that delivers function just as well as form.",
         images: {
@@ -190,7 +182,6 @@ const watchesArr = [
         id: 9,
         title: "Shinola The Runwell",
         price: 625.00,
-        quantity: 1,
         discount: 0,
         desc: "Our flagship style, the Runwell, is a simple and sophisticated watch crafted for decades of wear. This obsessively engineered watch is built to last and named accordingly. Two hands and a sub dial are driven by Shinola's Argonite 1069 high-accuracy quartz movement. The bold green dial is punctuated by applied Arabic numerals in white. The solid stainless steel case features a classic pumpkin crown with double-domed sapphire crystal to protect the watch from scratches throughout years of wear.",
         images: {
@@ -205,7 +196,6 @@ const watchesArr = [
         id: 10,
         title: "Tag Heuer Formula 1",
         price: 1650.00,
-        quantity: 1,
         discount: 87,
         desc: "The 43mm brushed steel case is fitted with a flat, virtually scratch-proof sapphire crystal. Pushbuttons at 2 o’clock and 4 o’clock are black PVD treated, as is the steel screw-down crown at 3 o'clock. “Easy Grip” finishes make all three easy to use, even when wearing racing gloves. The strap is made of high tech rubber with a brushed and engraved steel pin buckle.",
         images: {
@@ -220,7 +210,6 @@ const watchesArr = [
         id: 11,
         title: "MVMT Blacktop",
         price: 155.00,
-        quantity: 1,
         discount: 7,
         desc: "As bold as the open road. This oversized 47mm chronograph is inspired by 1960's auto culture, with double-domed glass and vintage dual dials. The Rallye Green matte gunmetal steel and olive colorway takes notes from that time's classic muscle car paint jobs.",
         images: {
@@ -235,7 +224,6 @@ const watchesArr = [
         id: 12,
         title: "Movado Museum Classic",
         price: 1295.00,
-        quantity: 1,
         discount: 0,
         desc: "Men's Museum Classic watch, 40 mm yellow gold PVD-finished stainless steel case, round black dial with 11 diamond markers (0.043 t.c.w.) and yellow gold-toned dot and hands, stainless steel and yellow gold PVD-finished link bracelet with deployment clasp.",
         images: {
@@ -249,7 +237,6 @@ const watchesArr = [
         id: 13,
         title: "Seiko",
         price: 350.00,
-        quantity: 1,
         discount: 0,
         desc: "Imported, Seiko's 4R35 mechanical movement beats at a frequency of 21,600 vibrations per hour with manual and automatic winding capabilities and a power reserve of 41 hours, Case Diameter: 39.9mm, Minimalist black dial with sunray finish and date calendar, Stainless steel case and bracelet with tri - fold push button release clasp, Water- resistant to 10 bar, 100 meters(330 feet), 3 - Year Manufacturer's Warranty",
         images: {
@@ -264,7 +251,6 @@ const watchesArr = [
         id: 14,
         title: "Citizen Attesa",
         price: 550.00,
-        quantity: 1,
         discount: 32,
         desc: "Powerful design and advanced technology drive the new luxury Attesa. Since its 1987 launch, Attesa incorporates titanium-processing know-how and features innovative, world-first technologies that will continue to shape the future. This 35th Anniversary model features the most advanced movement for Eco-Drive Satellite Wave GPS watches, and delivers the world’s fastest time-only signal reception in as little as 3 seconds and features a newly developed Duratect DLC Blue surface hardening technology on the bezel and bracelet.",
         images: {
@@ -278,7 +264,6 @@ const watchesArr = [
         id: 15,
         title: "Tissot Seastar",
         price: 525.00,
-        quantity: 1,
         discount: 0,
         desc: "Boasting water resistance to an underwater depth of nearly 1,000 feet, this rugged Swiss-made chronograph watch is built for divers and water-sports lovers. Set on a flexible mesh bracelet, the high-performance timepiece also features a unidirectional ceramic bezel and luminescent markers for easy readibility in low-light conditions.",
         images: {
